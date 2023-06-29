0f51425057a312a64f4a860529fe46581533c933
Dove-White <dugbazahredeemer@gmail.com>
2023-06-29 16:51:29 +0000

    responsive for header and main section one

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     extend: {},
+    screens: {
+      'sm': '414px',
+      'md': '908px',
+      'phone': '909px',
+      'xl': '1200px',
+    },
   },
   plugins: [],
 }
